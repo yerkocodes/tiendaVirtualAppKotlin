2757f626e6906500888381526f1d05eb448145c2
Yerko Cortés Baeza <holayerkocodes@gmail.com>
2025-04-10 20:35:07 -0400

    Dispositivo físico para pruebas

--- app/src/main/java/com/ykacorb/tiendavirtualapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/ykacorb/tiendavirtualapp_kotlin/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TiendaVirtualAppKotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "mundou",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hola $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TiendaVirtualAppKotlinTheme {
-        Greeting("Android")
+        Greeting("mundo")
     }
 }
