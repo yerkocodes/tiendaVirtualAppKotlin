c3bfdde740e91aa3fb3a4b9bf7bd7d42ebcc8a85
Yerko Cortés Baeza <holayerkocodes@gmail.com>
2025-04-10 20:01:43 -0400

    El nacimiento

---
+++ app/src/main/java/com/ykacorb/tiendavirtualapp_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ykacorb.tiendavirtualapp_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ykacorb.tiendavirtualapp_kotlin.ui.theme.TiendaVirtualAppKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TiendaVirtualAppKotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TiendaVirtualAppKotlinTheme {
+        Greeting("Android")
+    }
+}
